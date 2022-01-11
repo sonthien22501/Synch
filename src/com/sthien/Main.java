9927168e789de36b4f691f0aed8e995f83002feb
sonthien22501 <you@example.com>
2022-01-11 10:51:22 +0700

    Test

--- src/com/sthien/Main.java
+++ src/com/sthien/Main.java
@@ -6,11 +6,13 @@ public class Main {
         Account account = new Account(90);
         Customer customer1 = new Customer("Thien", account);
         Customer customer2 = new Customer("Nhi", account);
-
+        Customer customer3 = new Customer("Ro", account);
         Thread thread1 = new Thread(customer1);
         Thread thread2 = new Thread(customer2);
-
+        Thread thread3 = new Thread(customer3);
         thread1.start();
         thread2.start();
+        thread3.start();
+
     }
 }
