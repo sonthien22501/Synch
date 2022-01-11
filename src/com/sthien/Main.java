5f8d2c8a4bf6893c1206c80bcb2369fca31fb9fb
sonthien22501 <you@example.com>
2022-01-11 11:11:47 +0700

    Test2

--- src/com/sthien/Main.java
+++ src/com/sthien/Main.java
@@ -10,7 +10,6 @@ public class Main {
         Thread thread1 = new Thread(customer1);
         Thread thread2 = new Thread(customer2);
         Thread thread3 = new Thread(customer3);
-        int a = 4;
         thread1.start();
         thread2.start();
         thread3.start();
